f1e1726a72db4425889741861cfa98d22af782eb
Thomas Gasangwa <thomasgasangwa@gmail.com>
2024-01-04 20:07:50 +0200

    solve the insertion sort

--- traversy-js-challenges/09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
+++ traversy-js-challenges/09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
@@ -1,3 +1,13 @@
-function insertionSort() {}
+function insertionSort(arr) {
+  for(let i = 0; i < arr.length; i++){
+    let currentElement = arr[i]
+    let j = arr[i -1]
+
+    while(arr[j] >= 0 && arr[j] > currentElement){
+      
+    }
+  }
+  return arr
+}
 
 module.exports = insertionSort;
