0cb0d97c447faa52d9a69f28f7753ab2448aac8b
Thomas Gasangwa <thomasgasangwa@gmail.com>
2024-01-12 12:31:58 +0200

    refactor: solve the anagram booking challenge

--- traversy-js-challenges/06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ traversy-js-challenges/06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,3 +1,16 @@
-function anagramGrouping() {}
+function anagramGrouping(arr) {
+  const anagram = new Map()
+
+ for(let str of arr){
+   const orderedStr = str.split('').sort().join('')
+   if(anagram.has(orderedStr)){
+    anagram.set(orderedStr, [...anagram.get(orderedStr), str])
+   } else {
+    anagram.set(orderedStr, [str])
+   }
+
+ }
+ return Array.from(anagram.values())
+}
 
 module.exports = anagramGrouping;
