c5afcc782ceb6d0434dc4b19a5eccc96cdd91aaa
Thomas Gasangwa <thomasgasangwa@gmail.com>
2024-01-12 10:32:34 +0200

    refactor: solve the phone dictionary  number challenge

--- traversy-js-challenges/06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ traversy-js-challenges/06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,11 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(arr) {
+  const dictionary = new Map();
+
+  for(const data of arr){
+    const [key,value] = data.split(':');
+    dictionary.set(key, value);
+  }
+  return dictionary;
+}
 
 module.exports = phoneNumberDirectory;
