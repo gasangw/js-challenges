c26557a7a364a68482cfaecaa93eee8b7a155c65
Thomas Gasangwa <thomasgasangwa@gmail.com>
2024-01-12 12:11:37 +0200

    refactor: solve the word frequency counter challenge

--- traversy-js-challenges/06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ traversy-js-challenges/06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,18 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  const countedWords = new Map();
+
+  const lowerCaseStr = str.toLowerCase().split(/\W+/);
+
+  for(const word of lowerCaseStr){
+    if(word.trim() === '') continue;
+
+    if(countedWords.has(word)){
+      countedWords.set(word, countedWords.get(word) + 1)
+    } else {
+      countedWords.set(word, 1)
+    }
+  }
+  return countedWords;
+}
 
 module.exports = wordFrequencyCounter;
