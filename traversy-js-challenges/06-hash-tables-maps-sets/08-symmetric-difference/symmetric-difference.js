495351c8099066545aa1b0e872d82764fa957d5c
Thomas Gasangwa <thomasgasangwa@gmail.com>
2024-01-13 15:59:13 +0200

    refactor: solve the symmery challenge

--- traversy-js-challenges/06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ traversy-js-challenges/06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,7 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+   const newArr1 = arr1.filter((item) => !arr2.includes(item));
+   const newArr2 = arr2.filter((item) => !arr1.includes(item));
+   return newArr1.concat(newArr2);  
+}
 
 module.exports = symmetricDifference;
